35357c39701f9130ddc8b934f455244e5593b666
GopalanIyer <iyergopalan25@gmail.com>
2020-12-20 20:43:52 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -59,5 +59,23 @@
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
     }
   });
+  $('.testi3').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      1024: {
+        items: 3
+      }
+    }
+  });
+  
 
 })(jQuery); // End of use strict
